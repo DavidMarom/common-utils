104523ba8294b60df7250f526bdfc8e467c01d0b
David Marom <davidmarom@hotmail.com>
2022-07-29 18:51:50 +0300

    Fixed BFS

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
+![dm common utils](https://res.cloudinary.com/dojmo7vcc/image/upload/v1659037669/cujs3_jxygol.png)
 ![npm version](https://img.shields.io/npm/v/react-redux.svg?style=flat-square)
 # Common Utils
 Everyday functions you always need.
 
-## Install
+### Install
 ```npm i @davidmarom/dm-common-utils```
 
 ## Use
 ```js
-import * as cu from '@davidmarom/dm-common-utils';
+import  *  as  cu  from  '@davidmarom/dm-common-utils';
 console.log(cu.getCUVer());
 ```
 
-### getCUVer() - get current version
-### isLetter(char)
-### isNum(char)
-### saveJSONtoLocalStorage(key,JSON)
-### getJSONfromLocalStorage(key)
-### CenterVH(htmlELEMENT, width?, height? )
+- getCUVer() - get current version
+- isLetter(char)
+- isNum(char)
+- saveJSONtoLocalStorage(key, JSON)
+- getJSONfromLocalStorage(key)
+- findIntersection(arr1, arr2)
+- createBinaryTree()
+---
+
+### Binary Tree
+```js
+const  myTree = createBinaryTree();
+
+myTree.insert('value');
+myTree.find('value');
+myTree.BFD();
+```
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function CenterVH(el, w = "100%", h = "100%") {
 }
 
 function getCUVer() {
-    return '1.2.2';
+    return '1.4.2';
 }
 
 function isLetter(c) {
@@ -33,11 +33,101 @@ function getJSONfromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key));
 }
 
+function findIntersection(arr1, arr2) {
+    let a = arr1.split(', ')
+    let b = arr2.split(', ')
+    var result = a.filter(x => b.find(a => a === x))
+    return result.length > 0 ? result.join(',') : 'false'
+}
+
+// Binary Tree ==============================================
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function createBinaryTree() {
+    return new BinarySerachTree;
+}
+
+class BinarySerachTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // INSERT
+    insert(value) {
+        let newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+
+            while (true) {
+                if (value === current.value) return undefined;
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    // FIND
+    find(value) {
+        if (this.root === null) return false;
+        let current = this.root, found = false
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        } //while
+        if (!found) return undefined
+        return current;
+    }
+
+    // BFS - Breadth First Search
+    BFD() {
+        let data = [], queue = [], node = this.root;
+        queue.push(this.root);
+
+        while (queue.length) {
+            node = queue.shift()
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+} // end of class binary tree
+
 module.exports = {
+    CenterVH,
     getCUVer,
     isLetter,
     isNum,
     saveJSONtoLocalStorage,
     getJSONfromLocalStorage,
-    CenterVH
+    findIntersection,
+    createBinaryTree
 };
